232c28b8a312c92fbfb8b8de5fbc4a0d9d8e33db
Trong Nguyen <trong.nguyen@seltec.io>
2021-10-04 10:34:41 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 rm -rf build && mkdir build
 mvn clean install
 mvn javafx:jlink -pl :sockets-webproxy-client
-cp sockets-webproxy-common/target/*.jar build/
-cp sockets-webproxy-server/target/*.jar build/
+cp sockets-webproxy-server/target/*-jar-with-dependencies.jar build/
 cp sockets-webproxy-client/target/*.zip build/
